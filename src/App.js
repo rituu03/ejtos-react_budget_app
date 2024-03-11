aaa71d148bb7bb2ceba0c6135925e34140ae910f
rituu03 <ritusahu7890@gmail.com>
2024-03-11 23:10:46 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,11 @@ import Budget from './components/Budget';
 
 
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import ExpenseItem from './components/ExpenseItem';
+import AllocationForm from './components/AllocationForm';
 const App = () => {
     return (
         <AppProvider>
@@ -16,27 +21,27 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {
-                            /* Add Budget component here */
+                            <Budget/>
                         }        
 
                         {
-                            /* Add Remaining component here*/
+                            <Remaining/>
                         }        
 
                         {
-                            /* Add ExpenseTotal component here */
+                            <ExpenseTotal/>
                         }        
                        
                         {
-                            /* Add ExpenseList component here */
+                            <ExpenseList/>
                         }         
 
                         {
-                            /* Add ExpenseItem component here */
+                            <ExpenseItem/>
                         }        
 
                         {
-                            /* Add AllocationForm component here under */
+                            <AllocationForm/>
                         }        
 
                 </div>
